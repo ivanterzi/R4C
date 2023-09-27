d0901fc044f02fbff6f0ad8ffc6812b73ccdb976
Ivan Terzi <terzivan@icloud.com>
2023-09-27 16:36:21 +0300

    2

--- robots/tests.py
+++ robots/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+import json
 
-# Create your tests here.
+ # Create your tests here.
+from .models import Robot  # Замените на путь к вашей модели
+
+
+class CreateRobotTest(TestCase):
+     def test_create_robot(self):
+         # Создаем JSON-данные для запроса
+         data = {
+             'model': 'R2',
+             'version': 'D2',
+             'created': '2022-12-31 23:59:59'
+         }
+
+         # Отправляем POST-запрос к вашему API-endpoint
+         response = self.client.post(reverse('create_robot'), json.dumps(data), content_type='application/json')
+
+         # Проверяем, что получили ожидаемый статус код (201 - Created)
+         self.assertEqual(response.status_code, 201)
+
+         # Проверяем, что запись о роботе была создана в базе данных
+         self.assertTrue(Robot.objects.filter(model='R2', version='D2').exists())
+
+     def test_create_robot_invalid_data(self):
+         # Отправляем POST-запрос с неполными данными
+         data = {
+             'model': 'R2',
+             'version': 'D2'
+         }
+
+         # Отправляем POST-запрос с недостающими данными
+         response = self.client.post(reverse('create_robot'), json.dumps(data), content_type='application/json')
+
+         # Проверяем, что получили ожидаемый статус код (400 - Bad Request)
+         self.assertEqual(response.status_code, 400)
+
+         # Проверяем, что запись о роботе не была создана в базе данных
+         self.assertFalse(Robot.objects.filter(model='R2', version='D2').exists())
+     def test_create_robot_invalid_validation_data(self):
+         # Отправляем POST-запрос с неполными данными
+         data = {
+             'model': 'R2R',
+             'version': 'D2е',
+             'created': '2022-12-31T23:59:59Z',
+         }
+
+         # Отправляем POST-запрос с недостающими данными
+         response = self.client.post(reverse('create_robot'), json.dumps(data), content_type='application/json')
+
+         # Проверяем, что получили ожидаемый статус код (400 - Bad Request)
+         self.assertEqual(response.status_code, 400)
+
+         # Проверяем, что запись о роботе не была создана в базе данных
+         self.assertFalse(Robot.objects.filter(model='R2R', version='D2е').exists())
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,93 @@
+import json
+import re
+from datetime import datetime, timedelta, timezone
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from .models import Robot
+from django.http import JsonResponse, HttpResponse
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
+from urllib.parse import quote
+from collections import defaultdict
 
-# Create your views here.
+# Регулярное выражение для проверки формата модели (два символа, буквы и цифры)
+MODEL_REGEX = r'^[A-Z0-9]{2}$'
+
+
+@csrf_exempt
+def create_robot(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            model = data.get('model')
+            version = data.get('version')
+            created_str = data.get('created')
+
+            # Проверка формата модели с использованием регулярного выражения
+            if not re.match(MODEL_REGEX, model):
+                return JsonResponse({'error': 'Invalid model format'}, status=400)
+
+            if version and created_str:
+                try:
+                    # Попытка преобразовать строку даты в объект datetime
+                    created = datetime.strptime(created_str, '%Y-%m-%d %H:%M:%S')
+                except ValueError:
+                    return JsonResponse({'error': 'Invalid date format'}, status=400)
+
+                robot = Robot(model=model, version=version, created=created)
+                robot.save()
+                return JsonResponse({'message': 'Robot record created successfully'}, status=201)
+            else:
+                return JsonResponse({'error': 'Invalid data'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
+
+def get_robots(request):
+    if request.method == 'GET':
+        # Получите все уникальные модели роботов из базы данных
+        models = Robot.objects.values_list('model', flat=True).distinct()
+
+        # Создайте новую книгу Excel
+        workbook = Workbook()
+
+        # Для каждой модели создайте новый лист
+        for model in models:
+            worksheet = workbook.create_sheet(title=model)
+
+            # Заголовок для листа
+            worksheet.append(["Модель", "Версия", "Количество за неделю"])
+
+            # Получите роботов для данной модели из базы данных
+            robots = Robot.objects.filter(model=model)
+
+            # Создайте словарь для подсчета количества роботов по версиям
+            version_count = defaultdict(int)
+
+            # Определите начало и конец периода (неделя)
+
+            end_date = datetime.now(timezone.utc)
+            start_date = end_date - timedelta(days=7)
+
+            # Подсчитайте количество роботов за неделю
+            for robot in robots:
+                if start_date <= robot.created <= end_date:
+                    version_count[robot.version] += 1
+
+            # Заполните лист данными
+            for version, count in version_count.items():
+                worksheet.append([model, version, count])
+
+        # Удалите стандартный лист
+        del workbook[workbook.sheetnames[0]]
+        filename = "сводка_по_роботам.xlsx"
+        # Создайте HTTP-ответ для скачивания Excel-файла
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename="{quote(filename)}"'
+
+        # Сохраните книгу Excel в HTTP-ответ
+        workbook.save(response)
+
+        return response
